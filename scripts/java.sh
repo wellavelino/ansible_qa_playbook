e525057e4748091e95a0b2479eecdc9ea2249fa6
Wellington Avelino <wellington.avelino@outlook.com>
2020-03-05 13:41:37 +0000

    Remove bash profile

--- scripts/java.sh
+++ scripts/java.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # creates bash_profile if doesn't exists
-touch ~/.bash_profile
+touch ~/.zshrc
 
 #Setting Java Home
-echo "export JAVA_HOME=$(/usr/libexec/java_home)" >> ~/.bash_profile
+echo "export JAVA_HOME=$(/usr/libexec/java_home)" >> ~/.zshrc

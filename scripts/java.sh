beb0c4f18ead46509ac62423d0f6bfb5aeae001d
Wellington Avelino <wellington.avelino@outlook.com>
2021-06-30 11:25:48 +0200

    chore:(Adds JDK swap method)

--- scripts/java.sh
+++ scripts/java.sh
@@ -3,5 +3,12 @@
 # creates bash_profile if doesn't exists
 touch ~/.zshrc
 
+# Adds jdk changer method to zshrc.sh
+echo "jdk() {
+        version=$1
+        export JAVA_HOME=$(/usr/libexec/java_home -v"$version");
+        java -version
+ }" >>  ~/.zshrc
+
 #Setting Java Home
 echo "export JAVA_HOME=$(/usr/libexec/java_home)" >> ~/.zshrc
